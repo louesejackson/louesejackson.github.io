33b7217bd2530039f67342e9be3c878c78ae1c37
Sean Butler <butlersean@gmail.com>
2019-07-08 23:10:26 +0100

    EOD

--- js/my.js
+++ js/my.js
@@ -1,20 +1,20 @@
-// $(document).ready(function(){
-//    var scroll_start = 0;
-//    var startchange = $('#startchange');
-//    var navbar_height = $('.navbar').height() * 1.5;
-//    var offset = startchange.offset();
-//
-//    if ( offset ) {
-//      $(document).scroll(function() {
-//         scroll_start = $(this).scrollTop();
-//         if(scroll_start > offset.top - navbar_height) {
-//             $(".navbar").css('background-color', 'magenta');
-//          } else {
-//             $('.navbar').css('background-color', 'transparent');
-//          }
-//      });
-//    }
-//    else {
-//      $('.navbar').css('background-color', 'transparent');
-//    }
-// });
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var navbar_height = $('.navbar').height() * 1.5;
+   var offset = startchange.offset();
+
+   if ( offset ) {
+     $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > offset.top - navbar_height) {
+            $(".navbar").css('background-color', 'magenta');
+         } else {
+            $('.navbar').css('background-color', 'transparent');
+         }
+     });
+   }
+   else {
+     $('.navbar').css('background-color', 'transparent');
+   }
+});
